da550a3d999a15a06f555b96e9944709db44890f
Rafael <rafaelvmico@gmail.com>
2023-12-15 17:35:52 +0100

    renaming files for raluganplus

--- app/src/main/java/com/ralugan/raluganplus/ui/download/DownloadViewModel.kt
+++ app/src/main/java/com/ralugan/raluganplus/ui/download/DownloadViewModel.kt
@@ -1,13 +1,13 @@
-package com.ralugan.raluganplus.ui.notifications
+package com.ralugan.raluganplus.ui.download
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class NotificationsViewModel : ViewModel() {
+class DownloadViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is notifications Fragment"
+        value = "This is dashboard Fragment"
     }
     val text: LiveData<String> = _text
 }
