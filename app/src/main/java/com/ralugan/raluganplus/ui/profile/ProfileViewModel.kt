083ee83f330415747f090a017a42d19ba4f1c0d9
Rafael <rafaelvmico@gmail.com>
2023-12-16 18:56:06 +0100

    cleaning code for main menu

--- app/src/main/java/com/ralugan/raluganplus/ui/profile/ProfileViewModel.kt
+++ app/src/main/java/com/ralugan/raluganplus/ui/profile/ProfileViewModel.kt
@@ -1,13 +1,13 @@
-package com.ralugan.raluganplus.ui.search
+package com.ralugan.raluganplus.ui.profile
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class SearchViewModel : ViewModel() {
+class ProfileViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is notifications Fragment"
+        value = "This is Profile Fragment"
     }
     val text: LiveData<String> = _text
 }
