6b2f0ceb26877104b632415678fd5c7f37514265
Ganeche-S <sivasankar.ganeche@outlook.fr>
2023-12-28 17:56:23 +0100

    sign-up

--- app/src/main/java/com/ralugan/raluganplus/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/ralugan/raluganplus/ui/profile/ProfileFragment.kt
@@ -4,35 +4,59 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
+import com.google.firebase.auth.FirebaseAuth
 import com.ralugan.raluganplus.databinding.FragmentProfileBinding
 
 class ProfileFragment : Fragment() {
 
     private var _binding: FragmentProfileBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
+
+    private lateinit var emailEditText: EditText
+    private lateinit var passwordEditText: EditText
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val profileViewModel =
-            ViewModelProvider(this).get(ProfileViewModel::class.java)
-
         _binding = FragmentProfileBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textProfile
-        profileViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
+        emailEditText = binding.editTextEmail
+        passwordEditText = binding.editTextPassword
+
+        val signInButton: Button = binding.signInButton
+        signInButton.setOnClickListener {
+            val email = emailEditText.text.toString()
+            val password = passwordEditText.text.toString()
+
+            if (email.isNotEmpty() && password.isNotEmpty()) {
+                signUp(email, password)
+            } else {
+                Toast.makeText(requireContext(), "Veuillez remplir tous les champs", Toast.LENGTH_SHORT).show()
+            }
         }
+
         return root
+    }
+
+    private fun signUp(email: String, password: String) {
+        val auth = FirebaseAuth.getInstance()
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(requireActivity()) { task ->
+                if (task.isSuccessful) {
+                    // L'inscription a réussi
+                    Toast.makeText(requireContext(), "Inscription réussie", Toast.LENGTH_SHORT).show()
+                } else {
+                    // L'inscription a échoué
+                    Toast.makeText(requireContext(), "Erreur d'inscription: ${task.exception?.localizedMessage}", Toast.LENGTH_SHORT).show()
+                }
+            }
     }
 
     override fun onDestroyView() {
