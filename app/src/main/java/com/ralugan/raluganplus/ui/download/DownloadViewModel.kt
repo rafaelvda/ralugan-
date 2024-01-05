e97578bab18d6b5130dce727b19e098ba4c06424
Rafael <rafaelvmico@gmail.com>
2024-01-05 11:51:19 +0100

    finishing download ui

--- app/src/main/java/com/ralugan/raluganplus/ui/download/DownloadViewModel.kt
+++ app/src/main/java/com/ralugan/raluganplus/ui/download/DownloadViewModel.kt
@@ -7,7 +7,7 @@ import androidx.lifecycle.ViewModel
 class DownloadViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is dashboard Fragment"
+        value = "Vos téléchargements apparaîtront ici."
     }
     val text: LiveData<String> = _text
 }
