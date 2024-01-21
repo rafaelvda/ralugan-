0cf61d14d30881aaabe2f10bc5387951fdc9142b
Ganeche-S <sivasankar.ganeche@outlook.fr>
2024-01-21 18:40:38 +0100

    add HorizontalScrollView

--- app/src/main/java/com/ralugan/raluganplus/ui/search/SearchFragment.kt
+++ app/src/main/java/com/ralugan/raluganplus/ui/search/SearchFragment.kt
@@ -1,23 +1,15 @@
 package com.ralugan.raluganplus.ui.search
 
-import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.EditText
 import android.widget.ImageView
 import android.widget.SearchView
 import android.widget.TextView
-import androidx.core.content.ContentProviderCompat.requireContext
-import com.bumptech.glide.request.target.Target
 import androidx.fragment.app.Fragment
 import com.bumptech.glide.Glide
-import com.bumptech.glide.load.DataSource
-import com.bumptech.glide.load.engine.GlideException
-import com.bumptech.glide.request.RequestListener
 import com.ralugan.raluganplus.R
 import com.ralugan.raluganplus.api.ApiClient
 import com.ralugan.raluganplus.api.WikidataApi
@@ -28,8 +20,6 @@ import kotlinx.coroutines.launch
 import okhttp3.ResponseBody
 import org.json.JSONException
 import org.json.JSONObject
-import retrofit2.Call
-import retrofit2.Callback
 import retrofit2.Response
 
 class SearchFragment : Fragment() {
