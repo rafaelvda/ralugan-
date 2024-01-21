794baa25b6efbdf24f048d8503064e902a530e90
Ganeche-S <sivasankar.ganeche@outlook.fr>
2024-01-21 17:43:35 +0100

    display data from disney, marvel, starwars

--- app/src/main/java/com/ralugan/raluganplus/ui/home/HomeFragment.kt
+++ app/src/main/java/com/ralugan/raluganplus/ui/home/HomeFragment.kt
@@ -1,6 +1,9 @@
 package com.ralugan.raluganplus.ui.home
 
+import android.app.AlertDialog
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,7 +12,15 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.ralugan.raluganplus.R
+import com.ralugan.raluganplus.api.ApiClient
+import com.ralugan.raluganplus.api.WikidataApi
 import com.ralugan.raluganplus.databinding.FragmentHomeBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import okhttp3.ResponseBody
+import retrofit2.Response
+import androidx.navigation.fragment.findNavController
 
 class HomeFragment : Fragment() {
 
@@ -18,6 +29,8 @@ class HomeFragment : Fragment() {
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
+
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -43,6 +56,50 @@ class HomeFragment : Fragment() {
             textView.text = it
         }
         return root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val disneyButton = view.findViewById<ImageButton>(R.id.customButtonDisney)
+        val marvelButton = view.findViewById<ImageButton>(R.id.customButtonMarvel)
+        val starWarsButton = view.findViewById<ImageButton>(R.id.customButton4)
+        val pixarButton = view.findViewById<ImageButton>(R.id.customButton2)
+        val natGeoButton = view.findViewById<ImageButton>(R.id.customButton7)
+        val starplusButton = view.findViewById<ImageButton>(R.id.customButtonStar)
+
+        disneyButton.setOnClickListener {
+            findNavController().navigate(R.id.navigation_disney)
+        }
+        marvelButton.setOnClickListener {
+            findNavController().navigate(R.id.navigation_marvel)
+        }
+        starWarsButton.setOnClickListener {
+            findNavController().navigate(R.id.navigation_starwars)
+        }
+        pixarButton.setOnClickListener {
+            showUnderConstructionDialog()
+        }
+        natGeoButton.setOnClickListener {
+            showUnderConstructionDialog()
+        }
+        starplusButton.setOnClickListener {
+            showUnderConstructionDialog()
+        }
+    }
+
+    private fun showUnderConstructionDialog() {
+        // Créez une boîte de dialogue (AlertDialog) pour afficher le message "En construction"
+        val builder = AlertDialog.Builder(requireContext())
+        builder.setTitle("En cours de construction")
+        builder.setMessage("Nous travaillons sur cette fonctionnalité. Revenez bientôt pour les dernières mises à jour !")
+        builder.setPositiveButton("OK") { dialog, _ ->
+            dialog.dismiss()
+        }
+
+        // Affichez la boîte de dialogue
+        val dialog = builder.create()
+        dialog.show()
     }
 
     override fun onDestroyView() {
---
+++ app/src/main/java/com/ralugan/raluganplus/ui/home/StarWarsFragment.kt
@@ -0,0 +1,154 @@
+package com.ralugan.raluganplus.ui.home
+
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import com.bumptech.glide.Glide
+import com.ralugan.raluganplus.R
+import com.ralugan.raluganplus.api.ApiClient
+import com.ralugan.raluganplus.api.WikidataApi
+import com.ralugan.raluganplus.databinding.FragmentStarwarsBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import okhttp3.ResponseBody
+import org.json.JSONException
+import org.json.JSONObject
+import retrofit2.Response
+
+class StarWarsFragment : Fragment() {
+
+    private var _binding: FragmentStarwarsBinding? = null
+    private val binding get() = _binding!!
+
+    private val wikidataApi: WikidataApi = ApiClient.getWikidataApi()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentStarwarsBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val sparqlQuery = """
+            SELECT ?itemLabel ?pic
+            WHERE {
+            {
+                ?filmItem wdt:P1476 ?itemLabel. # Title
+                ?filmItem wdt:P31 wd:Q11424.  # Film
+                ?filmItem wdt:P750 wd:Q54958752.  # Platform = Disney+
+                ?filmItem wdt:P272 wd:Q242446. # LucasFilm
+
+                OPTIONAL {
+                ?filmItem wdt:P154 ?pic.
+                }
+            }
+            UNION
+            {
+                ?seriesItem wdt:P1476 ?itemLabel. # Title
+                ?seriesItem wdt:P31 wd:Q5398426.  # Television series
+                ?seriesItem wdt:P750 wd:Q54958752.  # Platform = Disney+
+                ?seriesItem wdt:P272 wd:Q242446. # LucasFilm
+
+                OPTIONAL {
+                ?seriesItem wdt:P154 ?pic.
+                }
+            }
+            }
+            ORDER BY DESC (?pic)
+        """.trimIndent()
+
+        // Appel de l'API dans une coroutine
+        CoroutineScope(Dispatchers.IO).launch {
+            try {
+                val response = wikidataApi.getDisneyPlusInfo(sparqlQuery, "json").execute()
+
+                activity?.runOnUiThread {
+                    handleApiResponse(response)
+                }
+            } catch (e: Exception) {
+                // Gérer l'exception
+            }
+        }
+    }
+
+    private fun handleApiResponse(response: Response<ResponseBody>) {
+        val linearLayout = binding.linearLayout
+
+        // Effacer les résultats précédents
+        linearLayout.removeAllViews()
+
+        if (response.isSuccessful) {
+            try {
+                val jsonResult = JSONObject(response.body()?.string())
+
+                if (jsonResult.has("results")) {
+                    val results = jsonResult.getJSONObject("results")
+                    val bindings = results.getJSONArray("bindings")
+
+                    if (bindings.length() > 0) {
+                        for (i in 0 until bindings.length()) {
+                            val binding = bindings.getJSONObject(i)
+                            val itemLabel = binding.getJSONObject("itemLabel").getString("value")
+
+                            // Créer un TextView pour le titre
+                            val titleTextView = TextView(requireContext())
+                            titleTextView.text = itemLabel
+
+                            // Créer un ImageView pour l'image
+                            if (binding.has("pic")) {
+                                val imageUrl = binding.getJSONObject("pic").getString("value").replace("http://", "https://")
+                                val imageView = ImageView(requireContext())
+
+                                // Utiliser Glide pour charger l'image dans l'ImageView
+                                Glide.with(this)
+                                    .load(imageUrl)
+                                    .error(R.drawable.ic_launcher_foreground)
+                                    .into(imageView)
+
+                                // Ajouter le TextView et ImageView au LinearLayout
+                                linearLayout.addView(titleTextView)
+                                linearLayout.addView(imageView)
+                            } else {
+                                // Si "pic" n'existe pas, ajouter seulement le TextView
+                                linearLayout.addView(titleTextView)
+                            }
+                        }
+                    } else {
+                        linearLayout.addView(createTextView("Aucun résultat trouvé"))
+                    }
+                } else {
+                    linearLayout.addView(createTextView("Aucun résultat trouvé"))
+                }
+            } catch (e: JSONException) {
+                linearLayout.addView(createTextView("Erreur de traitement JSON"))
+                Log.e("SearchFragment", "JSON parsing error: ${e.message}")
+            }
+        } else {
+            linearLayout.addView(createTextView("Erreur de chargement des données"))
+            Log.e("SearchFragment", "API call failed with code: ${response.code()}")
+            // ... (rest of the error handling)
+        }
+    }
+
+    private fun createTextView(text: String): TextView {
+        val textView = TextView(requireContext())
+        textView.text = text
+        return textView
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
